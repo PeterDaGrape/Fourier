4c67038bef66abe13466a35b67c72c022d509824
PeterDaGrape <petervine1@icloud.com>
2024-09-30 15:33:14 +0100

    Initial Commit

---
+++ Fourier/FourierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourierApp.swift
+//  Fourier
+//
+//  Created by Peter Vine on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FourierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
